77af6a8e737f6bce652d4abac50ba3884c053138
= <kristoffer@kristofferhebert.com>
2016-01-06 20:46:14 -0800

    finishing assignment

--- formValidation/app.js
+++ formValidation/app.js
@@ -16,27 +16,63 @@ match a character that isn't allowed in this password: /[^A-z0-9\!\@\#\$\%\^\&\*
 /*
 Grabbing a few inputs to help you get started...
  */
-var firstPasswordInput = document.querySelector('#first')
-var secondPasswordInput = document.querySelector('#second')
+var $firstPasswordInput = document.querySelector('#first')
+var $secondPasswordInput = document.querySelector('#second')
 var submit = document.querySelector('#submit')
 
 /*
 You'll probably find this function useful...
  */
 
-function noIllegalCharacters(selector){
-    return selector.value.match(/[^A-z0-9\!\@\#\$\%\^\&\*]/g)
+function minLength(value){
+    return (value.length >= 16) ? true : false
+}
+
+function maxLength(value){
+    return (value.length <= 100) ? true : false
+}
+
+function hasAUpperCaseLetter(value){
+    return (value.match(/[A-Z]/g)) ? true : false
+}
+
+function hasALowercaseLetter(value){
+    return (value.match(/[a-z]/g)) ? true : false
+}
+
+function hasASymbol(value){
+    return (value.match(/[\!\@\#\$\%\^\&\*]/g)) ? true : false
+}
+
+function hasANumber(value){
+    return (value.match(/[0-9]/g)) ? true : false
 }
 
 function passwordsMatch(selector1, selector2){
     return selector1.value === selector2.value
 }
 
-function handleSubmit(){
-    var bothInputMatch = passwordsMatch(firstPasswordInput, secondPasswordInput)
 
-    if(!bothInputMatch){
-        return secondPasswordInput.setCustomValidity('Passwords do not match')
+function validateValue(selector){
+    if(!hasAUpperCaseLetter(selector.value)) return selector.setCustomValidity('Password needs to contain a Uppercase Letter')
+    if(!hasALowercaseLetter(selector.value)) return selector.setCustomValidity('Password needs to contain a Lowercase Letter')
+    if(!hasASymbol(selector.value)) return selector.setCustomValidity('Password needs to contain a Symbol')
+    if(!hasANumber(selector.value)) return selector.setCustomValidity('Password needs to contain a Number')
+    if(!hasASymbol(selector.value)) return selector.setCustomValidity('Password needs to contain a Symbol')
+    if(!minLength(selector.value)) return selector.setCustomValidity('Password needs to at least 16 characters long')
+    if(!maxLength(selector.value)) return selector.setCustomValidity('Password cannot exceed 100 characters')
+
+    return selector.setCustomValidity('')
+
+}
+
+function handleSubmit(){
+
+    validateValue($firstPasswordInput)
+    validateValue($secondPasswordInput)
+
+    if(!passwordsMatch($firstPasswordInput, $secondPasswordInput)){
+        return $secondPasswordInput.setCustomValidity('Passwords do not match')
     }
 
 }
